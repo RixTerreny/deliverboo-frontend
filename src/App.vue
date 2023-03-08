4a15d953161cdafacb43f3d9c2d6cbfa5d74e661
RixTerreny <riccardotrave43@gmail.com>
2023-03-08 04:45:50 +0100

    draft home, fontawesome bug

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,70 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 </script>
 
 <template>
-
+<div class="container">
+  <h1 class="mb-5">Cerca il tuo piatto preferito!</h1>
+  <div class="row gap-4 text-white justify-content-center">
+    <div class="col-3 bg-black  pb-2">
+      <div><img src="" alt=""></div>
+      <i class="fa-solid fa-pizza"></i>
+      <div>Pizza</div>
+    </div>
+    <div class="col-3 bg-black pb-2">
+      <div><img src="" alt=""></div>
+      <i class="fa-solid fa-sushi"></i>
+      <div>Giapponese</div>
+    </div>
+    <div class="col-3 bg-black pb-2">
+      <div><img src="" alt=""></div>
+      <div>Vegetariano</div>
+    </div>
+    <div class="col-3 bg-black pb-2">
+      <div><img src="" alt=""></div>
+      <div>Dessert</div>
+    </div>
+    <div class="col-3 bg-black pb-2">
+      <div><img src="" alt=""></div>
+      <div>Americano</div>
+    </div>
+    <div class="col-3 bg-black pb-2">
+      <div><img src="" alt=""></div>
+      <div>Kebab</div>
+    </div>
+    <div class="col-3 bg-black pb-2">
+      <div><img src="" alt=""></div>
+      <div>Italiano</div>
+    </div>
+    <div class="col-3 bg-black pb-2">
+      <div><img src="" alt=""></div>
+      <div>Poke</div>
+    </div>
+    <div class="col-3 bg-black pb-2">
+      <div><img src="" alt=""></div>
+      <div>Messicano</div>
+    </div>
+    <div class="col-3 bg-black pb-2">
+      <div><img src="" alt=""></div>
+      <div>Indiano</div>
+    </div>
+  </div>
+</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss" scoped >
+@use './styles/general.scss' as *;
+
+i{
+    display: inline-block;
+    height: 1rem;
+    width: 1rem;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.col-3{
+  border-radius: 20px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.col-3:hover div{
+  color:red;
+  
 }
 </style>
