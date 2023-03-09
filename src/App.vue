12ff75161b7c174891db16821db67e86df51245c
RixTerreny <riccardotrave43@gmail.com>
2023-03-09 03:54:10 +0100

    rf 3 almost ended

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import HelloWorld from "./components/HelloWorld.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import { DOMDirectiveTransforms } from "@vue/compiler-dom";
 import { useAttrs } from "vue";
@@ -9,43 +9,48 @@ export default {
   pages: {},
   data() {
     return {
-      categories:[],
+      categories: [],
       restaurants: [],
-      filtered_restaurants: [],
-      filters:[],
+      filteredRestaurants: [],
+      filters: [],
     };
-
   },
   methods: {
     fetchdata() {
-      axios.get("http://127.0.0.1:8000/api/restaurants")
-        .then((resp) => {
-          this.categories = (resp.data.categories);
-          this.restaurants = (resp.data.restaurants);
-        })
+      axios.get("http://127.0.0.1:8000/api/restaurants").then((resp) => {
+        this.categories = resp.data.categories;
+        this.restaurants = resp.data.restaurants;
+      });
     },
-    search(){
-      setTimeout(()=>{
-        this.restaurants.forEach(restaurant =>{
-
-          this.filters.forEach(filtro => {
-            
-            restaurant.categories.forEach(categoria => {
-              if (filtro == categoria.pivot.category_id) {
-                this.filtered_restaurants.push(restaurant);
-              }
+    search() {
+      setTimeout(() => {
+        if ((this.filters === [0])) {
+          this.filteredRestaurants = this.restaurants;
+        }
+        else{
+          this.filteredRestaurants = [];
+          this.restaurants.forEach((restaurant) => {
+            this.filters.forEach((filtro) => {
+              restaurant.categories.forEach((categoria) => {
+                if (
+                  filtro == categoria.pivot.category_id &&
+                  !this.filteredRestaurants.some(
+                    (filteredRestaurant) =>
+                      filteredRestaurant.id === restaurant.id
+                  )
+                ) {
+                  this.filteredRestaurants.push(restaurant);
+                }
+              });
             });
           });
         }
-        );
-      },100)
-    }
+      }, 100);
+    },
   },
   mounted() {
     this.fetchdata();
-
-  }
-
+  },
 };
 </script>
 
@@ -53,16 +58,42 @@ export default {
   <div class="container">
     <h1 class="mb-5">Cerca il tuo piatto preferito!</h1>
     <div class="row gap-4 text-white justify-content-center">
-
-      <div class="col-3 bg-black pb-2" v-for="category in categories" @click="search">
-        <input type="checkbox" :value="category.id" v-model="filters">
+      <div
+        class="col-3 bg-black pb-2"
+        v-for="category in categories"
+        @click="search"
+      >
+        <input type="checkbox" :value="category.id" v-model="filters" />
         <div><img src="" alt="" /></div>
         <div>{{ category.name }}</div>
       </div>
-
     </div>
-  </div> 
-<RouterView :restaurants="restaurants"></RouterView>
+  </div>
+
+  <div class="row row-cols-4 g-5">
+    <div
+      class="col"
+      v-for="restaurant in filteredRestaurants"
+      :key="restaurant.id"
+    >
+      <div class="card bg-success h-100 w-100" style="width: 18rem">
+        <!-- <img
+            v-if="restaurant.cover_img"
+            class="card-img-top"
+            src="http://127.0.0.1:8000/ + '/storage/' + restaurant.cover_img)"
+            alt="Card image cap"
+          /> -->
+        <div class="card-body">
+          <h4 class="card-title">name: {{ restaurant.name }}</h4>
+          <p class="card-text">address: {{ restaurant.address }}</p>
+          <h5 class="card-text text-light">VAT: {{ restaurant.vat }}</h5>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- <RouterView :filtered_restaurants="filteredRestaurants"></RouterView> -->
 </template>
 
 <style lang="scss">
--- src/pages/Restaurants.vue
+++ src/pages/Restaurants.vue
@@ -1,16 +1,38 @@
 <script>
-import axios from "axios";
 export default {
   name: "index",
-  props: ["restaurants"],
+  props: ["filteredRestaurants"],
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+    search(){
+      setTimeout(()=>{
+        this.filteredRestaurants = [];
+        this.restaurants.forEach((restaurant) =>{
+
+          this.filters.forEach((filtro) => {
+
+            restaurant.categories.forEach((categoria) => {
+              if (filtro == categoria.pivot.category_id) {
+                return(this.filteredRestaurants.push(restaurant));
+              }
+            });
+          });
+        }
+        );
+      },100)
+    }
+  }
 };
 </script>
 
 <template>
-  <h1>mio htmllll</h1>
   <div class="container">
     <div class="row row-cols-4 g-5">
-      <div class="col" v-for="restaurant in restaurants" :key="restaurant.id">
+      <div class="col" v-for="restaurant in filteredRestaurants" :key="restaurant.id">
         <div class="card bg-success h-100 w-100" style="width: 18rem">
           <!-- <img
             v-if="restaurant.cover_img"
