85e12329ab53a0443c987b2b023dcd38bc6c601d
FilippoBid <FilippoPerrone1997@gmail.com>
2023-03-10 18:05:28 +0100

    .update

--- src/pages/Restaurants.vue
+++ src/pages/Restaurants.vue
@@ -15,10 +15,20 @@ export default {
       axios.get("http://127.0.0.1:8000/api/restaurants").then((resp) => {
         this.categories = resp.data.categories;
         this.restaurants = resp.data.restaurants;
-        this.search();
+       /*  this.search(); */
       });
     },
-    search() {
+    getFiltered($category) {
+      axios.get("http://127.0.0.1:8000/api/restaurants/category/" + $category)
+        .then((resp) => {
+          this.filteredRestaurants.push(resp.data.restaurants);
+  
+        });
+    },
+
+
+    
+    /* search() {
       setTimeout(() => {
         if (this.filters.length === 0) {
           this.filteredRestaurants = this.restaurants;
@@ -41,44 +51,38 @@ export default {
           });
         }
       }, 100);
-    },
+    } */
   },
   mounted() {
     this.fetchdata();
   },
 };
+
 </script>
 
 <template>
   <div class="container">
     <h1 class="my-5">Cerca il tuo piatto preferito!</h1>
     <div class="row gap-4 text-white justify-content-center">
-      <div
-        class="col-3 bg-black pb-2"
-        v-for="category in categories"
-        @click="search"
-      >
-        <input type="checkbox" :value="category.id" v-model="filters" />
+      <div class="col-3 bg-black pb-2" v-for="category in categories">
+        <!-- <input type="checkbox" :value="category.id" v-model="filters" />
         <div><img src="" alt="" /></div>
-        <div>{{ category.name }}</div>
+        <div>{{ category.name }}</div> -->
+        <button :key="category.id"  type="button" @click="getFiltered(category.id)">{{category.name}}</button>
       </div>
     </div>
   </div>
   <h1>Risultati:</h1>
   <div class="container">
     <div class="row row-cols-4 g-5">
-      <div
-        class="col"
-        v-for="restaurant in filteredRestaurants"
-        :key="restaurant.id"
-      >
+      <div class="col" v-for="restaurant in filteredRestaurants" :key="restaurant.id">
         <div class="card bg-success h-100 w-100" style="width: 18rem">
           <!-- <img
-            v-if="restaurant.cover_img"
-            class="card-img-top"
-            src="http://127.0.0.1:8000/ + '/storage/' + restaurant.cover_img)"
-            alt="Card image cap"
-          /> -->
+              v-if="restaurant.cover_img"
+              class="card-img-top"
+              src="http://127.0.0.1:8000/ + '/storage/' + restaurant.cover_img)"
+              alt="Card image cap"
+            /> -->
           <div class="card-body">
             <h4 class="card-title">name: {{ restaurant.name }}</h4>
             <p class="card-text">address: {{ restaurant.address }}</p>
@@ -90,7 +94,4 @@ export default {
   </div>
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
--- src/pages/dish/Dish.vue
+++ src/pages/dish/Dish.vue
@@ -30,6 +30,7 @@ export default {
         this.store.ShoppingCart.push(dish);
       }
         },
+    
   },
   mounted() {
     this.fetchdata();
