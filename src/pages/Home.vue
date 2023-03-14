4efb59ebd77dbf6ae98ca8b590209925744dfde2
TomasBelfonte <tomas.belfonte@gmail.com>
2023-03-14 17:18:41 +0100

    Fix Home Page Restaurants & categories

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,6 +8,7 @@ export default {
             restaurants: [],
             filteredRestaurants: [],
             filters: [],
+            filtercategory: []
         };
     },
     methods: {
@@ -21,9 +22,95 @@ export default {
         getFiltered($category) {
             axios.get("http://127.0.0.1:8000/api/restaurants/category/" + $category)
                 .then((resp) => {
-                    this.filteredRestaurants.push(resp.data.restaurants);
+                    this.filteredRestaurants = [];
+                    resp.data.restaurants.forEach(restaurant => {
+                        this.filteredRestaurants.push(restaurant);
+
+                    });
 
                 });
+        },
+        filterCategoryPush($category) {
+
+
+            if (this.filtercategory.length === this.categories.length) {
+                this.filtercategory = [];
+                this.filteredRestaurants = [];
+
+            }
+
+            if (!this.filtercategory.includes($category)) {
+
+                this.filtercategory.push($category);
+                this.filteredRestaurants = [];
+
+                this.filtercategory.forEach(element => {
+
+                    axios.get("http://127.0.0.1:8000/api/restaurants/category/" + element)
+                        .then((resp) => {
+
+                            resp.data.restaurants.forEach(restaurant => {
+                                if (this.filteredRestaurants.some(r => r.id === restaurant.id)) {
+
+                                    return;
+
+                                } else {
+                                    this.filteredRestaurants.push(restaurant);
+
+                                }
+
+                            });
+
+                        });
+
+
+                });
+            } else {
+                this.filtercategory.splice(this.filtercategory.indexOf($category), 1);
+                this.filteredRestaurants = [];
+
+                this.filtercategory.forEach(element => {
+
+                    axios.get("http://127.0.0.1:8000/api/restaurants/category/" + element)
+                        .then((resp) => {
+
+                            resp.data.restaurants.forEach(restaurant => {
+                                if (this.filteredRestaurants.some(r => r.id === restaurant.id)) {
+
+                                    return;
+
+                                } else {
+                                    this.filteredRestaurants.push(restaurant);
+
+                                }
+
+                            });
+
+                        });
+
+
+                });
+
+
+            }
+
+
+        },
+        getAllRestaurants() {
+            axios.get("http://127.0.0.1:8000/api/restaurants").then((resp) => {
+                this.filtercategory = [];
+                this.filteredRestaurants = [];
+                this.filteredRestaurants = resp.data.restaurants;
+                if (this.filtercategory.length === this.categories.length) {
+
+                } else {
+
+                    this.categories.forEach((category) => {
+                        this.filtercategory.push(category.id);
+                    });
+                }
+
+            });
         },
 
 
@@ -31,7 +118,7 @@ export default {
         search() {
             setTimeout(() => {
                 if (this.filters.length === 0) {
-                    this.filteredRestaurants = this.restaurants;
+                    this.filteredRestaurants = null;
                 } else {
                     this.filteredRestaurants = [];
                     this.restaurants.forEach((restaurant) => {
@@ -66,12 +153,36 @@ export default {
             <img src="public/img/deliveboo.png" class="logo margin-logo" alt="">
             <h3 class="ms-2 mt-4 text-orange">Scegli cosa vuoi mangiare</h3>
             <div class="row gap-4 text-white justify-content-center space-category-top space-category-bottom">
+                <!--  <h2 class="text-danger" v-if="(this.filtercategory != '' )|| (this.filtercategory.length == this.categories.length)">categorie attualmente selezionate: {{this.filtercategory}}</h2> -->
                 <div class="col-3" v-for="category in categories">
                     <!-- <input type="checkbox" :value="category.id" v-model="filters" />
               <div><img src="" alt="" /></div>
               <div>{{ category.name }}</div> -->
-                    <button class="text-orange font-cat bg-transparent" :key="category.id" type="button"
-                        @click="getFiltered(category.id)">{{ category.name }}</button>
+                    <button :class="{ 'bg-danger': this.filtercategory.includes(category.id) }" class="text-orange"
+                        :key="category.id" type="button" @click="filterCategoryPush(category.id)">{{ category.name }}</button>
+                </div>
+                <button class="text-orange font-cat bg-transparent" type="button" @click="getAllRestaurants()">tutti i
+                    ristoranti</button>
+            </div>
+
+        </div>
+    </div>
+
+    <h1 class="text-orange mb-5">Scegli il Ristorante:</h1>
+    <div class="container">
+        <div class="row row-cols-4 g-5">
+            <div class="col" v-for="restaurant in filteredRestaurants" :key="restaurant.id">
+                <div class="card bg-orange h-100 w-100" style="width: 18rem">
+                    <img v-if="restaurant.cover_img" class="card-img-top"
+                        src="http://127.0.0.1:8000/ + '/storage/' + restaurant.cover_img)" alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">{{ restaurant.name }}</h5>
+                        <h6 class="mt-5">Categorie:</h6>
+                        <div class="card-title" v-for="categor in restaurant.categories">{{ categor.name }}</div>
+                        <h6 class="mt-5">Indirizzo:</h6>
+                        <p class="card-text">{{ restaurant.address }}</p>
+                        <a :href="'/dish/' + restaurant.id" class="btn btn-light mt-4">Menù</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -116,5 +227,11 @@ export default {
 
 .margin-logo {
     margin-top: 30px;
+}
+
+.selectedbtn {
+
+    border-radius: 2px solid red;
+
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,16 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-
-import RestaurantsPage from "./pages/Restaurants.vue";
 import DishPage from "./pages/dish/Dish.vue";
 import HomePage from "./pages/Home.vue"
 const router = createRouter({
     history: createWebHistory(),
     routes:[
-        {
-            path:"/Restaurants",
-            name:"Restaurants",
-            component: RestaurantsPage,
-        },
         {
             path: "/dish/:id",
             name: "dish.index",
